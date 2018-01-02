954d5ff5b69e4b04bd6e9b5eaee94866450075e2
bwuning <bwuning@outlook.com>
2018-01-03 00:18:54 +0800

    完善构建中...

--- Client_simple/build/dev.js
+++ Client_simple/build/dev.js
@@ -1,21 +1,17 @@
 const exec = require('child_process').exec;
-
-
-
-
-// let webpack = exec('webpack ', (error, stdout, stderr)=> {
-//   if (error) {
-//     console.log(error.stack);
-//     console.log('Error code: ' + error.code);
-//   }
-//   console.log('Child Process STDOUT: ' + stdout);
-// });
-
  
-let koa = exec('node ../../Service/server/app.js', (error, stdout, stderr)=> {
+var ls = exec('cross-env NODE_ENV=development webpack-dev-server --open --hot', (error, stdout, stderr)=> {
   if (error) {
     console.log(error.stack);
     console.log('Error code: ' + error.code);
   }
   console.log('Child Process STDOUT: ' + stdout);
 });
+
+var ls = exec('cd ../Service && npm run dev', (error, stdout, stderr)=> {
+    if (error) {
+      console.log(error.stack);
+      console.log('Error code: ' + error.code);
+    }
+    console.log('Child Process STDOUT: ' + stdout);
+  });

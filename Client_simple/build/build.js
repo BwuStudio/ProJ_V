c1185032fa09042442b65d9f4bcb697466197b40
Bwuni <bwuni@outlook.com>
2018-01-03 10:14:29 +0800

    完善构建工具

--- Client_simple/build/build.js
+++ Client_simple/build/build.js
@@ -4,7 +4,7 @@ const
     ora = require('ora') // 控制台 loading 样式
     spinner = ora('building for production...')
 
-    path = require('path') // Path 模块
+    path = require('path').posix // Path 模块
     chalk = require('chalk') // 控制台 text 颜色
 
     //config = require('../config') //加载 config
--- Client_simple/build/dev.js
+++ Client_simple/build/dev.js
@@ -1,17 +1,26 @@
-const exec = require('child_process').exec;
- 
-var ls = exec('cross-env NODE_ENV=development webpack-dev-server --open --hot', (error, stdout, stderr)=> {
-  if (error) {
-    console.log(error.stack);
-    console.log('Error code: ' + error.code);
-  }
-  console.log('Child Process STDOUT: ' + stdout);
+const 
+    exec = require('child_process').exec,
+    spawn = require('child_process').spawn,
+    path = require('path')
+
+
+const 
+    service_path = path.resolve(__dirname, '../../Service'),
+    service = spawn(
+        'cmd.exe',['/s', '/c',`"cd ${service_path} && npm run dev"`], { cwd: null, env: null, windowsVerbatimArguments: true } 
+    )
+
+service.stdout.on('data', function (data) {
+    console.log(`service: ${data.toString()}`);
 });
 
-var ls = exec('cd ../Service && npm run dev', (error, stdout, stderr)=> {
-    if (error) {
-      console.log(error.stack);
-      console.log('Error code: ' + error.code);
-    }
-    console.log('Child Process STDOUT: ' + stdout);
-  });
+const 
+    client_path = path.resolve(__dirname,'../'),
+    client = spawn(
+        'cmd.exe',['/s', '/c',`"cd ${client_path} && webpack-dev-server --open --hot"`], { cwd: null, env: null, windowsVerbatimArguments: true } 
+    )
+
+client.stdout.on('data', function (buffer) {
+    console.log(`client: ${buffer.toString()}`);
+});
+
